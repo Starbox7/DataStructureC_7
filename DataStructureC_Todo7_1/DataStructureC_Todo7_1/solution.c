6f6f539c2ff60847c3ece591b118f6df2694a025
Starbox7 <arcione07@naver.com>
2022-05-19 10:08:11 +0900

    20220519 DataStructureC Queue

--- DataStructureC_Todo7_1/DataStructureC_Todo7_1/solution.c
+++ DataStructureC_Todo7_1/DataStructureC_Todo7_1/solution.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Q_SIZE 3
+#define Q_SIZE 5 //3
 
 typedef char element;
 
@@ -8,14 +8,14 @@ element queue[Q_SIZE];
 int front = -1, rear = -1;
 
 void isEmpty() {
-	printf("\n Queue is full! \n");
+	printf("\n Queue is empty! \n");
 }
 
 void isFull() {
 	printf("\n Queue is full! \n");
 }
 
-void enQueue(element item) {
+void enQueue(element item) {  //stack의 push() 함수와 유사
 	if (rear >= Q_SIZE - 1) {
 		isFull();
 		exit(1);
@@ -23,15 +23,15 @@ void enQueue(element item) {
 	else queue[++rear] = item;
 }
 
-element deQueue() {
-	if (front == rear) {
+element deQueue() { //stack의 삭제함수 pop()와 비교
+	if (front == rear) { // top == -1
 		isEmpty();
 		exit(1);
 	}
-	else return queue[++front];
+	else return queue[++front];  //data 반환 후 top 감소
 }
 
-void dek() {
+void del() {
 	if (front == rear) {
 		isEmpty();
 		exit(1);
@@ -57,6 +57,10 @@ void printQ() {
 }
 
 int main(void) {
+
+	printf("학번 : 18114847, 이름 : 김범준, 5/19 \n");
+	printf("실습#7-1, 선형큐에서의 삽입/삭제\n\n");
+
 	element data;
 
 	enQueue('A'); printQ();
@@ -68,7 +72,7 @@ int main(void) {
 
 	deQueue(); printQ();
 
-	enQueue('D'); printQ();
+	enQueue('D'); printQ(); //C D -> 입력불가(Queue Full) -> drift 현상 : 데이터가 앞에 비었음에도 들어가지 못함 (티켓 소진) / 예하 명령들 무의미됨
 
 	deQueue(); printQ();
 
